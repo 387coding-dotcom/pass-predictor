513438a5f55f5b6cc257e6706c3b7b61ed210445
Rushi <rushidhaigude528@gmail.com>
2026-09-04 18:44:13 +0530

    added forward pass function

--- network.js
+++ network.js
@@ -1,3 +1,13 @@
 function sigmoid(z){
     return 1/(1+ Math.exp(-z));
 }
+
+let w1 = 0.5; //importance of  hours studied
+let w2 = -0.3;// importance of hours slept
+let b = 0.1;
+
+function forward(study,sleep){
+    let z = w1*study+ w2*sleep + b;
+    let output = sigmoid(z);
+    return output;
+}
