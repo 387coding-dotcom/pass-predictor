0649649046439398bb3a929d01e5ccf529f381b9
Rushi <rushidhaigude528@gmail.com>
2026-09-04 18:51:17 +0530

    added training data in trainer.js

---
+++ trainer.js
@@ -0,0 +1,10 @@
+
+let trainingData = [
+  { study: 1, sleep: 1, passed: 0 },
+  { study: 2, sleep: 8, passed: 0 },
+  { study: 8, sleep: 2, passed: 0 },
+  { study: 5, sleep: 5, passed: 1 },
+  { study: 6, sleep: 6, passed: 1 },
+  { study: 9, sleep: 9, passed: 0 },
+  { study: 3, sleep: 3, passed: 0 }
+];
